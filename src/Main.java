deb4600bad7cc4e2cf836d02520fa2f8d28a7370
Peter Felix Nguyen <peterfelixnguyen@gmail.com>
2014-12-22 04:05:52 -0800

    Improved user interface. Javadoc + open license

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
+/**
+ * Copyright 2014 Latrice Sebastian, Peter "Felix" Nguyen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Toolkit;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -18,7 +33,17 @@ import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
 
+/**
+ * Starts the program
+ */
 public class Main {
     public static void main(String[] args) {
         Frame frame = new Frame();
@@ -26,9 +51,15 @@ public class Main {
     }
 }
 
+/**
+ * The frame of the program
+ */
 @SuppressWarnings("serial")
 class Frame extends JFrame {
 
+    /**
+     * Frame constructor
+     */
     public Frame() {
         add(new Panel());
         setJMenuBar(new MenuBar());
@@ -40,13 +71,19 @@ class Frame extends JFrame {
     }
 }
 
+/**
+ * The panel that contains the active calculator
+ */
 @SuppressWarnings("serial")
 class Panel extends JPanel {
     private PrecisionSlider jsBitMode = new PrecisionSlider();
-    private JTextField jtfInput = new JTextField(10);
-    private JButton jbCalculate = new JButton("Calculate");
+    private JTextField jtfInput = new JTextField(19);
     private JLabel jlOutput = new JLabel(" ");
+    private BinaryFractionCalc calc;
 
+    /**
+     * Panel constructor
+     */
     public Panel() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         JPanel jpUpper = new JPanel(new GridLayout(1,2));
@@ -68,7 +105,6 @@ class Panel extends JPanel {
 
         JPanel container3 = new JPanel(new FlowLayout(FlowLayout.LEFT));
         container3.add(jtfInput);
-        container3.add(jbCalculate);
         jpRightInUpper.add(container3);
 
         JPanel jpLower = new JPanel(new GridLayout(2,1));
@@ -79,27 +115,141 @@ class Panel extends JPanel {
         container4.add(jlOutput);
         jpLower.add(container4);
 
-        jbCalculate.addActionListener(new ActionListener() {
+        /* Detect changes in Bit-Mode and perform new calculation */
+        jsBitMode.addChangeListener(new ChangeListener() {
 
             @Override
-            public void actionPerformed(ActionEvent e) {
-                BinaryFractionCalc calc = new BinaryFractionCalc(new Double(jtfInput.getText()));
-                if (jsBitMode.getValue() == 1) {
-                    jlOutput.setText(calc.getHalf());
-                } else if (jsBitMode.getValue() == 2) {
-                    jlOutput.setText(calc.getSingle());
-                } else {
-                    jlOutput.setText(calc.getDouble());
+            public void stateChanged(ChangeEvent e) {
+                if (jtfInput.getText().length() > 0) {
+
+                    calc = new BinaryFractionCalc(new Double(jtfInput.getText()));
+
+                    if (jsBitMode.getValue() == 1) {
+                        jlOutput.setText(calc.getHalf());
+                    } else if (jsBitMode.getValue() == 2) {
+                        jlOutput.setText(calc.getSingle());
+                    } else {
+                        jlOutput.setText(calc.getDouble());
+                    }
                 }
-            }});
+            }
+        });
+
+        /* Automatically perform the calculation based on current value of text field */
+        jtfInput.getDocument().addDocumentListener(new DocumentListener() {
+
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                if (jtfInput.getText().length() > 0) {
+
+                    calc = new BinaryFractionCalc(new Double(jtfInput.getText()));
+
+                    if (jsBitMode.getValue() == 1) {
+                        jlOutput.setText(calc.getHalf());
+                    } else if (jsBitMode.getValue() == 2) {
+                        jlOutput.setText(calc.getSingle());
+                    } else {
+                        jlOutput.setText(calc.getDouble());
+                    }
+                }
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                if (jtfInput.getText().length() > 0) {
+
+                    calc = new BinaryFractionCalc(new Double(jtfInput.getText()));
+
+                    if (jsBitMode.getValue() == 1) {
+                        jlOutput.setText(calc.getHalf());
+                    } else if (jsBitMode.getValue() == 2) {
+                        jlOutput.setText(calc.getSingle());
+                    } else {
+                        jlOutput.setText(calc.getDouble());
+                    }
+                }
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+            }
+        });
+
+
+        /* Unfinished Implementation of DocumentFilter */
+
+        /*
+        AbstractDocument inputDoc = (AbstractDocument) jtfInput.getDocument();
+        inputDoc.setDocumentFilter(new InputFilter(jsBitMode, jlOutput, jtfInput));
+         */
+    }
+}
+
+/**
+ * Unfinished DocumentFilter intended to filter (block) specific input values
+ */
+class InputFilter extends DocumentFilter {
+    private JSlider jsBitMode;
+    private JLabel jlOutput;
+    private JTextField jtfInput;
+    private Pattern pattern; // implement later
+
+    @SuppressWarnings("unused")
+    private InputFilter() {
+        // no default constructor
+    }
+
+    public InputFilter(JSlider jsBitMode, JLabel jlOutput, JTextField jtfInput) {
+        this.jsBitMode = jsBitMode;
+        this.jlOutput = jlOutput;
+        this.jtfInput = jtfInput;
+        pattern = Pattern.compile("([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])");
+    }
+
+    public void processBits() {
+        BinaryFractionCalc calc = new BinaryFractionCalc(new Double(jtfInput.getText()));
+        if (jsBitMode.getValue() == 1) {
+            jlOutput.setText(calc.getHalf());
+        } else if (jsBitMode.getValue() == 2) {
+            jlOutput.setText(calc.getSingle());
+        } else {
+            jlOutput.setText(calc.getDouble());
+        }
+    }
+
+    // manual invocation
+    @Override
+    public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+        String newStr = fb.getDocument().getText(0, fb.getDocument().getLength()) + string;
+        Matcher m = pattern.matcher(newStr);
+
+        if (m.matches()) {
+            super.insertString(fb, offset, string, attr);
+        } else {
+            Toolkit.getDefaultToolkit().beep();
+        }
+    }
+
+    // automatic invocation
+    @Override
+    public void remove(DocumentFilter.FilterBypass fb, int offset, int length) throws BadLocationException {
+        super.remove(fb, offset, length);
+    }
+
+    // automatic invocation
+    @Override
+    public void replace(FilterBypass fb, int offset, int length, String string, AttributeSet attr) throws BadLocationException {
+        if (length > 0) {
+            fb.remove(offset, length);
+        }
+
+        insertString(fb, offset, string, attr);
     }
 }
 
 /**
  * The PrecisionSlider is a JSlider used to configure the precision-mode
  * for calculation of IEEE-754 fractions.
- * 
- * @author Peter Nguyen
  */
 @SuppressWarnings("serial")
 class PrecisionSlider extends JSlider {
@@ -128,8 +278,6 @@ class PrecisionSlider extends JSlider {
  * allows the user to switch calculators. The edit menu allows the user to modify or
  * access the output. The options menu contains output and input preferencees. The
  * help menu contains information on the calculator and the developers.
- * 
- * @author Peter Nguyen
  */
 @SuppressWarnings("serial")
 class MenuBar extends JMenuBar {
